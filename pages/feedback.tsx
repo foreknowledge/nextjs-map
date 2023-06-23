7d00076a5edd2a0e6fa647a5cb8edab1cbdca785
foreknowledge <kyj_0129@naver.com>
2023-06-23 15:18:17 +0900

    Header UI

---
+++ pages/feedback.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/common/Header';
+import { Fragment } from 'react';
+
+export default function Feedback() {
+  return (
+    <Fragment>
+      <Header />
+      <main></main>
+    </Fragment>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,28 @@
 import Header from '@/components/common/Header';
+import styles from '@/styles/header.module.scss';
+import Link from 'next/link';
 import { Fragment } from 'react';
+import { AiOutlineShareAlt } from 'react-icons/ai';
+import { VscFeedback } from 'react-icons/vsc';
 
 export default function Home() {
   return (
     <Fragment>
-      <Header />
+      <Header
+        rightElements={[
+          <button
+            onClick={() => alert('복사!')}
+            className={styles.box}
+            style={{ marginRight: '8px' }}
+            key="button"
+          >
+            <AiOutlineShareAlt size={20} />
+          </button>,
+          <Link href="/feedback" className={styles.box} key="link">
+            <VscFeedback size={20} />
+          </Link>,
+        ]}
+      />
       <main></main>
     </Fragment>
   );
