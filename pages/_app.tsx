990a6f058878bf7033344f76f658a686d324d32d
foreknowledge <kyj_0129@naver.com>
2023-06-28 16:10:31 +0900

    config: Add scripts for google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,22 @@ import '@/styles/globals.scss';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import SEO from '@/seo.config';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-MBE1G4VTDQ" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MBE1G4VTDQ');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
